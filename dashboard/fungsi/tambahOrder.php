79def4cd45d7f2094edb4969df3779c7c2360440
DyakaFaiz <dyakafaiz123@gmail.com>
2023-12-16 18:43:52 +0700

    fix bug

--- dashboard/fungsi/tambahOrder.php
+++ dashboard/fungsi/tambahOrder.php
@@ -19,16 +19,20 @@ if ($meja < 1) {
   return false;
 }
 
-mysqli_query($kon, "UPDATE tb_detail_order set status_dorde = '' WHERE id_order = '$id_order'");
+mysqli_query($kon, "UPDATE tb_detail_order set status_dorde = 1 WHERE id_order = '$id_order'");
 
 mysqli_query($kon, "UPDATE tb_meja set status = 1 WHERE meja_id = '$meja'");
 
+$queryTambah = "INSERT INTO tb_order VALUES('$id_order', '$meja', '$tanggal', '$tanggal2', '$user_id', '$keterangan', 0)";
+$query = mysqli_query($kon, $queryTambah);
+
 if ($query > 0) {
   $_SESSION['pesan'] = '
-  <div class="alert alert-success mb-2 alert-dismissible text-small " role="alert">
-    <b>Yoi!</b> Pesanan sedang diproses, mohon tunggu sampai masakan datang
-    <button type="button" class="close" data-dismiss="alert" aria-label="close"><span aria-hidden="true">&times;</span></button>
-  </div>';
+		<div class="alert alert-success mb-2 alert-dismissible text-small " role="alert">
+			<b>Yoi!</b> Pesanan sedang diproses, mohon tunggu sampai masakan datang
+			<button type="button" class="close" data-dismiss="alert" aria-label="close"><span aria-hidden="true">&times;</span></button>
+		</div>
+	';
   header('location:../index.php');
 } else {
   $_SESSION['pesan'] = '
